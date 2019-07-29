a39569546555fab984f2b8bea7ac40415b151f6f
Yu Zhang <memo40k@outlook.com>
2019-07-28 20:06:23 -0400

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -10,6 +10,9 @@ from app import app
 # such nodes as well.
 CONNECTED_NODE_ADDRESS = "http://127.0.0.1:8000"
 
+grass_ID=0
+
+
 posts = []
 
 
@@ -43,6 +46,20 @@ def index():
                            node_address=CONNECTED_NODE_ADDRESS,
                            readable_time=timestamp_to_string)
 
+@app.route('/download',methods=['GET'])
+def download():
+    #把chain写进md文件
+    print ("download")
+    fo = open("backup_chain.md", "w") 
+    fo.write(str(get_chain(CONNECTED_NODE_ADDRESS + '/chain')))
+    fo.close()
+    return redirect('/')
+
+
+@app.route('/search',methods=['POST'])
+def search(hash_number): 
+    #
+    return True
 
 @app.route('/submit', methods=['POST'])
 def submit_textarea():
@@ -52,8 +69,14 @@ def submit_textarea():
     post_content = request.form["content"]
     author = request.form["author"]
     quantity = request.form["quantity"]
+    if author == "Farm":
+        global grass_ID
+        grass_ID += 1
+    else:
+        grass_ID = int(request.form["grass_ID"])
 
     post_object = {
+        'grass_ID': grass_ID,
         'author': author,
         'content': post_content,
         'quantity': quantity
@@ -65,15 +88,28 @@ def submit_textarea():
     requests.post(new_tx_address,
                   json=post_object,
                   headers={'Content-type': 'application/json'})
-    #提交之后自动挖矿
+    #auto mining the transaction block after submit
     miner_url = CONNECTED_NODE_ADDRESS + "/mine"
     auto_miner(miner_url)
     return redirect('/')
 
-#自动挖矿与验证
-def auto_miner(url_address):
-    miner=requests.get(url_address)
+#mine latest block
+def auto_miner(url_mine):
+    miner=requests.get(url_mine)
     print("auto miner working:",miner.status_code," ",miner.content)
 
+#get the whole chain data
+def get_chain(url_chain):
+    chain = requests.get(url_chain)
+    if chain.status_code == 200: #把chain.content改成Jason文件，按时间顺序排列
+        chain_file=chain.content
+        my_json = (chain.content).decode('utf8').replace("'", '"')
+        # Load the JSON to a Python list & dump it back out as formatted JSON
+        data = json.loads(my_json)
+        return (data['chain'])
+    else:
+        return False
+
+#tiimestamp to string
 def timestamp_to_string(epoch_time):
     return datetime.datetime.fromtimestamp(epoch_time).strftime('%D %H:%M:%S')
